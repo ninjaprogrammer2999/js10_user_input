35888cb8e2e9db29a353ce6937595dd0ec8a4d0b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-19 22:47:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // User input!***
-/*
+
 confirm("Ok === true \n cancel === false");
 console.log(confirm);
+
 // let's store this in a variable */
-/*
+
 let input = confirm("Ok === true \n cancel === false");
 console.log(input);
-*/
 ////////////////////////////////////////////////////////////////////////
 // 3rd type // prompt 
 //prompt("enter your name:");
 // so let's store this input in a variable***
-/*
+
 let name = prompt("enter your name");
 console.log(name);
-*/
+
 // if we press cancel we get a null value on console
 // we can't do the same thing with a null as with strings**
 // null could also translate as false, zero or undifined ****
@@ -23,26 +23,25 @@ console.log(name);
 // by using *** nullish coalescing operator ***
 ///////////////////////////////////////////////////////////
 // nullish coalescing operator (??) ****
-/*
+
 let name = prompt ("enter your name:");
 console.log(name ?? "you did'nt enter your name.");
-*/
 ////////////////////////////////////////////////////////////
-/*
+
 let name = prompt("enter your name.");
 if (name) {
     console.log(name?? "you didn't enter your name.");
 } else {
     console.log("you didn't enter your name.");
-} */
+} 
 ////////////////////////////////////////////////////////
-/*
+
 let name = prompt ("enter your name.");
 if (name) {
     console.log(name);
 } else {
     console.log("you didn't enter your name.");
-} */
+}
 //////////////////////////////////////////////////////////
 let name = prompt("enter your name.");
 if (name) {
@@ -53,7 +52,7 @@ if (name) {
 } else {
     console.log("you didn't enter your name.");
 }
-///////////////////////////////////////////////////////
+
 
 
 
